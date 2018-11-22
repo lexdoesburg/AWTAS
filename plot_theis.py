c96525571860317e02600488b2a23170681f2205
Lex Doesburg <41774000+lexdoesburg@users.noreply.github.com>
2018-11-22 13:04:37 +1300

    Some changes in Theis solution

--- plot_theis.py
+++ plot_theis.py
@@ -4,24 +4,24 @@ import matplotlib.pyplot as plt
 # Page 11 AWTAS
 p0 = 3.6e6 # Pa
 h = 100 # m
-r = 0.5 # m
-q = 0.005 # m^3/s
+r = 0.05 # m
+qm = -0.005 # m^3/s
 k = 10e-13 # m^2
-porosity = 0.1
-density = 813.37 # Water at 240 degrees celsius
-v = 0.0001111 # Water at 240 degrees celsius
+phi = 0.1
+rho = 813.37 # Water at 240 degrees celsius
+nu = 0.0001111 # Water at 240 degrees celsius
 C = 0.001303 # Water at 240 degrees celsius
 t = np.linspace(1, 43200, num=100)
 
-p = theis_solution(p0, q, k, h, porosity, density, v, C, r, t)
+p = theis_solution(p0, qm, k, h, phi, rho, nu, C, r, t)
 
-noise_sd = 0.005
-p += p * noise_sd * np.random.randn(p.shape[0])
+# noise_sd = 0.005
+# p += p * noise_sd * np.random.randn(p.shape[0])
 
 f, ax1 = plt.subplots(nrows=1, ncols=1)
 
-ax1.plot(t/3600, p/1e5)
-ax1.set_xlabel("Time (hours)")
+ax1.plot(np.log(t/3600), p/1e5)
+ax1.set_xlabel("Log Time (hours)")
 ax1.set_ylabel("Pressure (bar)")
 
 plt.show()
