dbe0203ce947cd6a94bd9f91f07b888fffb2f46c
Lex Doesburg <41774000+lexdoesburg@users.noreply.github.com>
2018-11-22 17:03:15 +1300

    Cleaning up code

--- testing_theis.py
+++ testing_theis.py
@@ -12,7 +12,7 @@ rho = 813.37 # Water at 240 degrees celsius
 nu = 0.0001111 # Water at 240 degrees celsius
 C = 0.001303 # Water at 240 degrees celsius
 time = 43200 # seconds
-t = np.linspace(1, 43200, num=100)
+t = np.linspace(0, 43200, num=100)
 
 p = theis_solution(p0, qm, k, h, phi, rho, nu, C, r, t)
 
@@ -26,17 +26,18 @@ data = generate_data(phi, k, 100, time, p0, qm, h, rho, nu, C, r)
 noisey_data = generate_data(phi, k, 100, time, p0, qm, h, rho, nu, C, r, noise=True, sd=1e-3)
 print(data)
 print(noisey_data)
-
 # f, ax1 = plt.subplots(nrows=1, ncols=1)
 # ax1.plot(t, data, "k-", label="Theis Data")
 # ax1.plot(t, noisey_data, "g-", label="Synthetic Data")
 # ax1.set_xlabel("Time (s)")
 # ax1.set_ylabel("Pressure (Pa)")
-
+# ax1.legend(loc="best")
 # plt.show()
 
 # Test if non-linear optimisation working
-phi, k = find_model_parameters(noisey_data, p0, qm, h, rho, nu, C, r, t, phi=0.0002, k=2.3e-14)
+# phi = input("Enter porosity: ")
+# k = input("Enter permeability: ")
+phi, k = find_model_parameters(noisey_data, p0, qm, h, rho, nu, C, r, t, phi=2., k=2.3e-12)
 print(phi)
 print(k)
 
@@ -45,6 +46,7 @@ approximated_data = generate_data(phi, k, 100, time, p0, qm, h, rho, nu, C, r)
 
 plt.plot(t, noisey_data,"k-",label="Synthetic Data (Theis Solution W/Noise)")
 plt.plot(t, approximated_data,"r-",label="Approximated Curve")
+plt.plot(t, p, "g-", label="Theis Analytic Solution")
 plt.title("Observed Data vs Fitted Curve")
 plt.xlabel("Time (s)")
 plt.ylabel("Pressure (Pa)")
