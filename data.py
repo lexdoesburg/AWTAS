45a586e8abf08b0c2d96d47cd8cdcbd5c55a3d68
Lex Doesburg <lex.doesburg3@gmail.com>
2018-11-25 19:47:34 +1300

    Looking at GUI layout and data class

---
+++ data.py
@@ -0,0 +1,5 @@
+# from numpy import genfromtxt
+
+class Data():
+    def __init__(self, filename):
+        pass
--- trialing_code.py
+++ trialing_code.py
@@ -1,80 +1,144 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
+# #!/usr/bin/env python
+# #-*- coding:utf-8 -*-
+
+# import random
+
+# from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+# from matplotlib.figure import Figure
+
+# from PyQt5 import QtCore #conda install pyqt
+# from PyQt5 import QtWidgets
+
+# class MatplotlibWidget(QtWidgets.QWidget):
+#     def __init__(self, parent=None):
+#         super(MatplotlibWidget, self).__init__(parent)
+
+#         self.figure = Figure()
+#         self.canvas = FigureCanvasQTAgg(self.figure)
+
+#         self.axis = self.figure.add_subplot(111)
+
+#         self.layoutVertical = QtWidgets.QVBoxLayout(self)#QVBoxLayout
+#         self.layoutVertical.addWidget(self.canvas)
+
+# class ThreadSample(QtCore.QThread):
+#     newSample = QtCore.pyqtSignal(list)
+
+#     def __init__(self, parent=None):
+#         super(ThreadSample, self).__init__(parent)
+
+#     def run(self):
+#         randomSample = random.sample(range(0, 10), 10)
+
+#         self.newSample.emit(randomSample)
+
+# class MyWindow(QtWidgets.QWidget):
+#     def __init__(self, parent=None):
+#         super(MyWindow, self).__init__(parent)
+
+#         self.pushButtonPlot = QtWidgets.QPushButton(self)
+#         self.pushButtonPlot.setText("Plot")
+#         self.pushButtonPlot.clicked.connect(self.on_pushButtonPlot_clicked)
+
+#         self.matplotlibWidget = MatplotlibWidget(self)
+
+#         self.layoutVertical = QtWidgets.QVBoxLayout(self)
+#         self.layoutVertical.addWidget(self.pushButtonPlot)
+#         self.layoutVertical.addWidget(self.matplotlibWidget)
+
+#         self.threadSample = ThreadSample(self)
+#         self.threadSample.newSample.connect(self.on_threadSample_newSample)
+#         self.threadSample.finished.connect(self.on_threadSample_finished)
+
+#     @QtCore.pyqtSlot()
+#     def on_pushButtonPlot_clicked(self):
+#         self.samples = 0
+#         self.matplotlibWidget.axis.clear()
+#         self.threadSample.start()
+
+#     @QtCore.pyqtSlot(list)
+#     def on_threadSample_newSample(self, sample):
+#         self.matplotlibWidget.axis.plot(sample)
+#         self.matplotlibWidget.canvas.draw()
+
+#     @QtCore.pyqtSlot()
+#     def on_threadSample_finished(self):
+#         self.samples += 1
+#         if self.samples <= 2:
+#             self.threadSample.start()
+
+# if __name__ == "__main__":
+#     import sys
+
+#     app = QtWidgets.QApplication(sys.argv)
+#     app.setApplicationName('MyWindow')
+
+#     main = MyWindow()
+#     main.resize(666, 333)
+#     main.show()
+
+#     sys.exit(app.exec_())
+
+
+import sys
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QGridLayout
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
 
 import random
 
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
-from matplotlib.figure import Figure
-
-from PyQt5 import QtCore #conda install pyqt
-from PyQt5 import QtWidgets
-
-class MatplotlibWidget(QtWidgets.QWidget):
+class Window(QDialog):
     def __init__(self, parent=None):
-        super(MatplotlibWidget, self).__init__(parent)
+        super(Window, self).__init__(parent)
 
-        self.figure = Figure()
-        self.canvas = FigureCanvasQTAgg(self.figure)
+        # a figure instance to plot on
+        self.figure = plt.figure()
 
-        self.axis = self.figure.add_subplot(111)
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
 
-        self.layoutVertical = QtWidgets.QVBoxLayout(self)#QVBoxLayout
-        self.layoutVertical.addWidget(self.canvas)
+        # this is the Navigation widget
+        # it takes the Canvas widget and a parent
+        self.toolbar = NavigationToolbar(self.canvas, self)
 
-class ThreadSample(QtCore.QThread):
-    newSample = QtCore.pyqtSignal(list)
+        # Just some button connected to `plot` method
+        self.button = QPushButton('Plot')
+        self.button.clicked.connect(self.plot)
 
-    def __init__(self, parent=None):
-        super(ThreadSample, self).__init__(parent)
+        # set the layout
+        layout = QGridLayout()
+        layout.addWidget(self.toolbar, 0, 0)
+        layout.addWidget(self.canvas, 1, 0)
+        layout.addWidget(self.button, 0, 1)
+        self.setLayout(layout)
 
-    def run(self):
-        randomSample = random.sample(range(0, 10), 10)
+    def plot(self):
+        ''' plot some random stuff '''
+        # random data
+        data = [random.random() for i in range(10)]
 
-        self.newSample.emit(randomSample)
+        # instead of ax.hold(False)
+        self.figure.clear()
 
-class MyWindow(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super(MyWindow, self).__init__(parent)
+        # create an axis
+        ax = self.figure.add_subplot(111)
 
-        self.pushButtonPlot = QtWidgets.QPushButton(self)
-        self.pushButtonPlot.setText("Plot")
-        self.pushButtonPlot.clicked.connect(self.on_pushButtonPlot_clicked)
+        # discards the old graph
+        # ax.hold(False) # deprecated, see above
 
-        self.matplotlibWidget = MatplotlibWidget(self)
+        # plot data
+        ax.plot(data, '*-')
 
-        self.layoutVertical = QtWidgets.QVBoxLayout(self)
-        self.layoutVertical.addWidget(self.pushButtonPlot)
-        self.layoutVertical.addWidget(self.matplotlibWidget)
+        # refresh canvas
+        self.canvas.draw()
 
-        self.threadSample = ThreadSample(self)
-        self.threadSample.newSample.connect(self.on_threadSample_newSample)
-        self.threadSample.finished.connect(self.on_threadSample_finished)
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
 
-    @QtCore.pyqtSlot()
-    def on_pushButtonPlot_clicked(self):
-        self.samples = 0
-        self.matplotlibWidget.axis.clear()
-        self.threadSample.start()
-
-    @QtCore.pyqtSlot(list)
-    def on_threadSample_newSample(self, sample):
-        self.matplotlibWidget.axis.plot(sample)
-        self.matplotlibWidget.canvas.draw()
-
-    @QtCore.pyqtSlot()
-    def on_threadSample_finished(self):
-        self.samples += 1
-        if self.samples <= 2:
-            self.threadSample.start()
-
-if __name__ == "__main__":
-    import sys
-
-    app = QtWidgets.QApplication(sys.argv)
-    app.setApplicationName('MyWindow')
-
-    main = MyWindow()
-    main.resize(666, 333)
+    main = Window()
     main.show()
 
     sys.exit(app.exec_())
