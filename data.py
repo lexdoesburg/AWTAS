12cfc5558e78cb9465726028b4160591bac5965c
Lex Doesburg <41774000+lexdoesburg@users.noreply.github.com>
2018-11-26 10:37:56 +1300

    Making data structure

--- data.py
+++ data.py
@@ -1,5 +1,53 @@
-# from numpy import genfromtxt
+# import numpy as np
+from numpy import genfromtext
 
 class Data():
-    def __init__(self, filename):
-        pass
+    def __init__(self, filename=None, time=None, observed=None, parameters=None):
+        self.filename = filename
+        self.time = time # list of time data
+        self.observed = observed # list of pressure data
+        self.approximation = None
+        # self.p0 = None
+        # self.qm = None
+        # self.h = None
+        # self.rho = None
+        # self.nu = None
+        # self.C = None
+        # self.r = None
+        self.parameters = parameters #list of the parameters
+        self.phi = None
+        self.k = None
+        
+        if filename:
+            self.read_file()
+    
+    def read_file(self):
+        if self.filename:
+            self.time, self.observed = genfromtxt(self.filename, delimiter=',', skip_header=1).T
+    
+    def set_known_parameters(self, parameters):
+        """
+        parameters = list of parameters
+        parameters[0] = intial pressure
+        parameters[1] = mass flowrate
+        parameters[2] = thickness
+        parameters[3] = density
+        parameters[4] = kinematic viscosity
+        parameters[5] = compressibility
+        parameters[6] = radius
+        """
+        self.parameters = parameters
+
+    def set_unknown_parameters(self, phi, k):
+        self.phi = phi
+        self.k = k
+
+    def set_time(self, time):
+        self.time = time
+    
+    def set_pressure(self, pressure):
+        self.pressure = pressure
+    
+    def set_approximation(self, approximation):
+        self.approximation = approximation
+
--- model.py
+++ model.py
@@ -5,16 +5,19 @@ from scipy.optimize import leastsq
 
 from datetime import datetime
 
+import data as data_class
+
 class Model():
-    def __init__(self, p0, qm, h, rho, nu, C, r, t, data=None):
-        self.p0 = p0 # Initial pressure
-        self.qm = qm # Mass flowrate (constant, -ve for production)
-        self.h = h # Thickness
-        self.rho = rho # Density
-        self.nu = nu # Kinematic viscosity
-        self.C = C # Compressibility
-        self.r = r # Radius
-        self.t = t # 1D array of measurement times
+    def __init__(self, data=None):
+    # def __init__(self, p0, qm, h, rho, nu, C, r, t, data=None):
+        # self.p0 = p0 # Initial pressure
+        # self.qm = qm # Mass flowrate (constant, -ve for production)
+        # self.h = h # Thickness
+        # self.rho = rho # Density
+        # self.nu = nu # Kinematic viscosity
+        # self.C = C # Compressibility
+        # self.r = r # Radius
+        # self.t = t # 1D array of measurement times
         self.data = data # Observed pressure/time measurements
 
     def model(self, phi, k):
@@ -25,7 +28,7 @@ class Model():
         Calculate the residual of the model (difference between observed data and estimated data)
         """
         phi, k = parameters
-        return self.data - self.model(phi, k)
+        return self.data.observed() - self.model(phi, k)
 
     def find_model_parameters(self, phi=0.1, k=1e-14, curve_fit=False):
         if curve_fit:
@@ -35,40 +38,46 @@ class Model():
             initial_parameters = np.array([phi, k])
             optimal_parameters, flag = leastsq(self.residual_function, initial_parameters)
             phi, k = optimal_parameters
+        self.data.set_unknown_parameters(phi, k) # Store phi and k in data structure
+        self.data.set_approximation(self.model(phi, k)) # Store the approximated data in the data structure
         return phi, k
 
-    def generate_data(self, phi, k, noise = False, sd = 2.5e-4, save_file=False, filename="example_datafile.txt"):
+    def generate_data(self, phi, k, time, noise = False, sd = 2.5e-4, save_file=False, filename="example_datafile.txt"):
         """
         Generate approximated data using the Theis solution for a guess of porosity and permeability.
         """
         # filename="datafile_{}.txt".format(datetime.now().strftime("%d-%M-%Y_%H:%M") # Argument
+        self.data = data_class.Data()
+        self.data.set_time(time)
         p = self.model(phi, k)
         if noise:
-            np.random.seed(0) # Set random seed to 0 for consistancy in testing
+            np.random.seed(0) # Set random seed to 0 for consistency in testing
             p += p*sd*np.random.randn(p.shape[0])
 
         if save_file:
             self.__generate_datafile(filename, p)
-        self.data = p
+        self.data.set_pressure(p)
         return self.data
     
     def __generate_datafile(self, filename, measurement):
         with open(filename, 'w') as file:
             file.write('Time(s),Pressure(Pa)\n')
-            for i in range(len(self.t)):
-                if self.t[i] >= 1e-7:
-                    file.write('{},{}\n'.format(self.t[i], measurement[i]))
+            for i in range(len(self.data.time)):
+                if self.data.time[i] >= 1e-7:
+                    file.write('{},{}\n'.format(self.data.time[i], measurement[i]))
                 else:
                     file.write('{},{}\n'.format(0, measurement[i]))
 
 
 class Theis_Solution(Model):
     def model(self, phi, k):
-        D = k/(self.nu*phi*self.rho*self.C)
+        p0, qm, h, rho, nu, C, r = self.data.parameters
+        D = k/(nu*phi*rho*C)
         # u = np.divide(r**2,4*D*t, out=np.zeros_like(t), where = t != 0)
         with np.errstate(divide="ignore", invalid="ignore"): # Hides 'RuntimeWarning: invalid value encountered in divide' if t[0] == 0.
-            p = self.p0 + (self.qm/(self.r*np.pi*k*(self.h/self.nu)))*exp1((self.r**2)/(4*D*self.t)) # Double check exponential integral is correct
+            p = p0 + (qm/(r*np.pi*k*(h/nu)))*exp1((r**2)/(4*D*self.data.time)) # Double check exponential integral is correct
             # p = p0 + ((qm*nu)/(4*np.pi*k*h))*exp1((r**2)/(4*D*t)) # Double check exponential integral is correct
-        if self.t[0] <= 1e-7: # Check if initial reading is at time 0
-            p[0] = self.p0
+        if self.data.time[0] <= 1e-7: # Check if initial reading is at time 0
+            p[0] = p0
+        # self.data.set_approximation(p)
         return p
