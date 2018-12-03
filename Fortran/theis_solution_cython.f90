0e1ef7141d24365ba17540306b15996b5ce0dd5a
Lex Doesburg <41774000+lexdoesburg@users.noreply.github.com>
2018-12-04 10:10:19 +1300

    Testing if functions work in fortran

--- Fortran/theis_solution_cython.f90
+++ Fortran/theis_solution_cython.f90
@@ -7,7 +7,7 @@ module theis_solution
     contains
 
 ! -----------------------------------------------------------------------------
-    function AnalyticalTheis(variable)
+    function AnalyticalTheis(variable, k, phi, P0, Q0, h, rho, nu, C, r, nObservations, time)
   
 !   This uses the Theis curve to calculate pressure at each (r,t) using the 
 !   given variables.  Analytic solution due to Mike O'Sullivan.
@@ -27,92 +27,83 @@ module theis_solution
 !   1: k (permeability)
 !   2: phi (porosity)
 
-    use problem_data
+    ! use problem_data
 
 !   Argument Variables:
     real(DP),intent(in) :: variable(:)
-    real(DP) :: AnalyticalTheis(TotalNData)
+    real(DP),intent(in) :: k !
+    real(DP),intent(in) :: phi
+    real(DP),intent(in) :: P0 !
+    real(DP),intent(in) :: Q0 !qm
+    real(DP),intent(in) :: h !
+    real(DP),intent(in) :: rho
+    real(DP),intent(in) :: nu
+    real(DP),intent(in) :: C
+    real(DP),intent(in) :: r
+    integer(I4B),intent(in) :: nObservations
+    real(DP),intent(in),dimension(nObservations) :: time
+    real(DP),intent(out) :: AnalyticalTheis(nObservations)
+    
+    integer(I4B) :: i
+    real(DP) :: pressure
+    real(DP) :: t
 
-!   Local Variables:
-!   Fixed parameters:	           
-    real(DP) :: nu,rho,B,C 
-!   Reservoir conditions:
-	real(DP) :: P0
-!   Variable parameters:
-    real(DP) :: k,phi 
-!   Local variables:
     real(DP) :: CC,D,x ! Intermediate quantities
-    real(DP) :: r,t,pressure,flow,Q0
-    integer(I4B) :: ObsPointNo,i,ObsPointOffset,DataNo
-	real(DP) :: DataOffset
-
-!   Unpack fixed parameters:
-    B=FixedParameter(2)
-    nu=FixedParameter(4)
-    rho=FixedParameter(5)
-    C=FixedParameter(6)
-!   Unpack reservoir conditions:
-    P0=ReservoirCondition(1)
-!   Unpack variable parameters:
-    k=variable(1)
-    phi=variable(2)
-      
-!   Get pumping rate (only one constant-rate pump):
-    Q0=PumpSchemeParams(1,1)
     
     CC=Q0*nu/(4.0_dp*pi_d*B*k)
     D=k/(nu*phi*rho*C)
 
-!   Loop over ObsPoints:
-    do ObsPointNo=1,NObsPoints
+! !   Loop over ObsPoints:
+!     do ObsPointNo=1,NObsPoints
     
-        ObsPointOffset=ObsPoint(ObsPointNo)%DataIndex-1
-        r=ObsPoint(ObsPointNo)%Position%x(1)
-	    DataOffset=ObsPoint(ObsPointNo)%DataOffset
+!         ObsPointOffset=ObsPoint(ObsPointNo)%DataIndex-1
+!         r=ObsPoint(ObsPointNo)%Position%x(1)
+! 	    DataOffset=ObsPoint(ObsPointNo)%DataOffset
       
-        select case (ObsPoint(ObsPointNo)%Property)
+!         select case (ObsPoint(ObsPointNo)%Property)
       
-        case(0)  ! Flow:
+!         case(0)  ! Flow:
         
-!            Loop over datapoints:
-            do i=1,ObsPoint(ObsPointNo)%NData
+! !            Loop over datapoints:
+!             do i=1,ObsPoint(ObsPointNo)%NData
       
-                DataNo=ObsPointOffset+i
-                t=TestData(DataNo)%time
+!                 DataNo=ObsPointOffset+i
+!                 t=TestData(DataNo)%time
         
-                if (t >= small_D) then        
-                    x=0.25_dp*r**2.0_dp/(D*t)          
-                    flow=Q0*dexp(-x)
-                else  ! t<=0
-                    flow=0.0_DP
-                end if
+!                 if (t >= small_D) then        
+!                     x=0.25_dp*r**2.0_dp/(D*t)          
+!                     flow=Q0*dexp(-x)
+!                 else  ! t<=0
+!                     flow=0.0_DP
+!                 end if
             
-                AnalyticalTheis(DataNo)=flow-DataOffset
+!                 AnalyticalTheis(DataNo)=flow-DataOffset
 
-            end do  ! Datapoint loop
+!             end do  ! Datapoint loop
  
-            case(1)  ! Pressure:
+!             case(1)  ! Pressure:
 
-!           Loop over datapoints:
-            do i=1,ObsPoint(ObsPointNo)%NData
+! !           Loop over datapoints:
+    do i=1,nObservations
+
+        ! DataNo=ObsPointOffset+i
+        ! t=TestData(DataNo)%time
+        t = time(i)
+
+        if (t >= small_D) then        
+            x=0.25_dp*r**2.0_dp/(D*t)          
+            pressure=P0+CC*E1(x)
+        else  ! t<=0
+            pressure=P0
+        end if
+    
+        AnalyticalTheis(i)=pressure
+
+    end do  ! Datapoint loop
+
+    !     end select          
       
-                DataNo=ObsPointOffset+i
-                t=TestData(DataNo)%time
-        
-                if (t >= small_D) then        
-                    x=0.25_dp*r**2.0_dp/(D*t)          
-                    pressure=P0+CC*E1(x)
-                else  ! t<=0
-                    pressure=P0
-                end if
-            
-                AnalyticalTheis(DataNo)=pressure-DataOffset
-
-            end do  ! Datapoint loop
-
-        end select          
-      
-    end do  ! ObsPoint loop      
+    ! end do  ! ObsPoint loop      
     
     return
     
