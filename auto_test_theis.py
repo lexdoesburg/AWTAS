83b6a02c46e8dfb8b6bbf89944c0e692531ed961
Lex Doesburg <41774000+lexdoesburg@users.noreply.github.com>
2019-01-14 11:49:01 +1300

    Writing an automatic testcase for theis solution

---
+++ auto_test_theis.py
@@ -0,0 +1,28 @@
+import pytest
+import numpy as np
+import model
+import data
+
+def get_expected_output(test_file):
+    with open(test_file, 'r') as file:
+        file.next() # Skip header
+        parameters_string = file.readline()
+        parameters = [float(value) for value in parameters_string.split(',')]
+        file.next() # Skip more metadata
+        variables_string = file.readline()
+        variables = [float(value) for value in parameters_string.split(',')]
+    time, expected_pressure = np.genfromtxt(test_file, delimiter=',', skip_header=5).T
+    return variables, parameters, time, expected_pressure
+
+def get_actual_output(variables, parameters, time):
+    theis_data = data.Data(time=time, parameters=parameters)
+    theis_model = model.Theis_Solution(theis_data)
+    actual_pressure = theis_model.model(variables)
+    return actual_pressure
+
+@pytest.mark.parametrize('test_file', ['theis_testcase1.txt', 'theis_testcase2.txt'])
+
+def test_theis_output(test_file):
+    variables, parameters, time, expected_pressure = get_expected_output(test_file)
+    actual_pressure = get_actual_output(variables, parameters, time)
+    assert np.allclose(actual_pressure, expected_pressure)
