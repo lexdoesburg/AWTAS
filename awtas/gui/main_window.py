3bf32c19a93f0b04da0993cd7e4ea941dcd8df09
Lex Doesburg <41774000+lexdoesburg@users.noreply.github.com>
2019-03-09 23:19:05 +1300

    Added menu bar to GUI main window

--- awtas/gui/main_window.py
+++ awtas/gui/main_window.py
@@ -1,5 +1,6 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
 
 import awtas.gui.tab_widget as tab_widget
 
@@ -7,17 +8,53 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.title = 'AWTAS'
-        # self.plot_widget = PlotWidget(self)
-        self.tabs = tab_widget.Tabs(self)
+        self.tab_widget = tab_widget.Tabs(self)
         self.init_UI()
         
     def init_UI(self):
         self.setWindowTitle(self.title)
-        # self.setCentralWidget(self.plot_widget)
-        self.setCentralWidget(self.tabs)
-        self.show()
+        self.setCentralWidget(self.tab_widget)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MainWindow()
-    sys.exit(app.exec_())
+        menu_bar = self.menuBar()
+        menu_bar.setNativeMenuBar(False)
+        file_menu = menu_bar.addMenu('&File')
+        
+        # Import data action
+        import_data = QAction('&Import Data', self)
+        import_data.setShortcut('Ctrl+L')
+        import_data.setStatusTip('Import data from file')
+        import_data.triggered.connect(self.import_data)
+
+        # Export/save data action
+        export_data = QAction('&Export Results', self)
+        export_data.setShortcut('Ctrl+S')
+        export_data.setStatusTip('Export results to file')
+        export_data.triggered.connect(self.export_results)
+
+        # Exit action adapted from http://zetcode.com/gui/pyqt5/menustoolbars/
+        exit = QAction('&Exit', self)
+        exit.setShortcut('Ctrl+Q')
+        exit.setStatusTip('Exit application')
+        exit.triggered.connect(self.close)
+
+        # Add file bar actions
+        file_menu.addAction(import_data)
+        file_menu.addAction(export_data)
+        file_menu.addAction(exit)
+        
+        self.showMaximized() # self.show()
+
+    def get_current_plot_widget(self):
+        current_tab = self.tab_widget.tabs.currentIndex()
+        current_plot_widget = self.tab_widget.tabs.widget(current_tab)
+        return current_plot_widget
+
+    @pyqtSlot()
+    def import_data(self):
+        current_plot_widget = self.get_current_plot_widget()
+        current_plot_widget.plot_data()
+
+    @pyqtSlot()
+    def export_results(self):
+        current_plot_widget = self.get_current_plot_widget()
+        current_plot_widget.produce_output_file()
--- run_gui.py
+++ run_gui.py
@@ -1,7 +1,10 @@
+"""
+Running this script will start the GUI application
+"""
 if __name__ == '__main__':
     import sys
     from PyQt5.QtWidgets import QApplication
-    
+
     import awtas.gui.main_window as main_window
     
     app = QApplication(sys.argv)
